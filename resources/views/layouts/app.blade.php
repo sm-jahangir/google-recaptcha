6f1f821b955a860bae5ba28a81648357542333d3
Jahangir Alam <samraatjahangir@gmail.com>
2022-12-03 23:57:06 +0600

    google recapture added confirm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-
+    {!! NoCaptcha::renderJs() !!}
 </head>
 <body>
     <div id="app">
